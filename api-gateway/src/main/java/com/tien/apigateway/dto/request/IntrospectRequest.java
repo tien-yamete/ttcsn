fe89f03ddd1b976245e10b7f7246bc1aa737c21e
tien-yamete <tavantien786@gmail.com>
2025-09-16 11:25:44 +0700

    add api gateway

---
+++ api-gateway/src/main/java/com/tien/apigateway/dto/request/IntrospectRequest.java
@@ -0,0 +1,15 @@
+package com.tien.apigateway.dto.request;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class IntrospectRequest {
+    String token;
+}
---
+++ api-gateway/src/main/java/com/tien/apigateway/dto/response/IntrospectResponse.java
@@ -0,0 +1,12 @@
+package com.tien.apigateway.dto.response;
+
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class IntrospectResponse {
+    boolean valid;
+}
