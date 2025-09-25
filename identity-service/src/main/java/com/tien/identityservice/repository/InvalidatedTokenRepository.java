14e980e036c70d6234031246c6d6e358894e5f43
tien-yamete <tavantien786@gmail.com>
2025-09-25 19:58:44 +0700

    remove all files from frontend branch

--- identity-service/src/main/java/com/tien/identityservice/repository/InvalidatedTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tien.identityservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.tien.identityservice.entity.InvalidatedToken;
-
-@Repository
-public interface InvalidatedTokenRepository extends JpaRepository<InvalidatedToken, String> {}
--- identity-service/src/main/java/com/tien/identityservice/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tien.identityservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.tien.identityservice.entity.Role;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, String> {}
