adba4928a78715dd88d42659f09ac631769e0901
tavt-dev <tavantien786@gmail.com>
2025-11-19 23:36:35 +0700

    feat: add social service with friendship feature

---
+++ social-service/src/main/java/com/tien/socialservice/SocialServiceApplication.java
@@ -0,0 +1,15 @@
+package com.tien.socialservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class SocialServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialServiceApplication.class, args);
+    }
+
+}
